f942ebeb88d6f6885cb873bb8434d6470af5580f
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-07-14 10:42:08 -0700

    bug fixes

--- models/base_model.py
+++ models/base_model.py
@@ -3,16 +3,34 @@
 
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel():
-    """The base class of the model"""
+    """The base class of the model
 
-    def __init__(self):
+    *args:a list of arguments
+    **kwargs: dict of Key-values of the args
+
+    """
+
+    def __init__(self, *args, **kwargs):
         """initializes all the public attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key in kwargs:
+                if key == 'created_at':
+                    self.__dict__['created_at'] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """returns string representation of an object"""
@@ -22,6 +40,7 @@ class BaseModel():
     def save(self):
         """updates the public instance attribute updated_at"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """ returns a dict containing all keys/values of dict"""
