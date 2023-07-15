a4cc4cafef50e53fb597d6080ec543bc42218479
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-07-15 06:22:42 -0700

    new classes

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""class Amenity that inherits from BaseModel"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """defines a class amenity"""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""class State that inheerits from BaseModel"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """class tha defines a state"""
+    name = ""
